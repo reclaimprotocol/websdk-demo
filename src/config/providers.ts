7ec5a3f17a2740a55c144bdcc0608bf914914587
kryptocodes <srivatsantb@gmail.com>
2025-12-12 01:16:41 +0530

    chore: update providerIds
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/config/providers.ts
+++ src/config/providers.ts
@@ -5,7 +5,23 @@ export interface Provider {
 
 // Add your providers here
 export const providers: Provider[] = [
-   {
+  {
+    providerId: "e6a203d8-33d6-47dc-a2aa-6a49e3a954ba",
+    providerName: "Kaggle"
+  },
+  {
+    providerId: "6d3f6753-7ee6-49ee-a545-62f1b1822ae5",
+    providerName: "Github"
+  },
+  {
+    providerId: "636848af-0f31-4d1c-998f-4cc8c44c09b9",
+    providerName: "Uber"
+  },
+  {
+    providerId: "1f6da24f-6ca2-4cfa-9bf7-392c62cb1687",
+    providerName: "Instagram"
+  },
+  {
     providerId: "d504a779-f6af-42b5-acc0-6c776b4ad348",
     providerName: "IGNOU"
   },
